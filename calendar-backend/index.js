02844171951409e794cdd3bcd67bf6e3861d5b21
alpetrovich <alexispetrovich11@gmail.com>
2022-08-29 10:03:38 -0300

    config express

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,1 +1,18 @@
-console.log("hola mundo!!");
+const express = require('express');
+
+//Crear el servidor de express
+const app = express();
+
+//rutas
+app.get('/', (req, res) =>{
+
+    console.log('se requiere /');
+    res.json({
+        ok: true,
+    })
+})
+
+//Escuchar peticiones
+app.listen(4000, ()=>{
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
