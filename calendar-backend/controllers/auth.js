4874b5a991ffa6907961cb26579156b9a21a89ad
alpetrovich <alexispetrovich11@gmail.com>
2022-08-30 14:58:44 -0300

    [add] model & user

--- calendar-backend/controllers/auth.js
+++ calendar-backend/controllers/auth.js
@@ -1,17 +1,26 @@
 const { response } = require('express');
+const User = require('../models/User');
 
 //* create user
-const createUser = (req, res = response) =>{
+const createUser = async(req, res = response) =>{
 
-    const { name, email, password } = req.body;
-
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
+    // const { name, email, password } = req.body;
+    try {
+        const user = new User(req.body);
+    
+        await user.save();
+    
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador.',
+        })
+    }
 }
 
 
